0df456d6e23d167f2e4f63d560011e22a97e2cd6
Kevin Seghetti <kts@tenetti.org>
2021-06-08 21:54:59 -0700

    first added, terraform configuration to create aws ecs container with docker container running in it

---
+++ app.json.tpl
@@ -0,0 +1,45 @@
+[
+  {
+    "name": "${prefix}-app",
+    "image": "${aws_ecr_repository}:${tag}",
+    "essential": true,
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-region": "${region}",
+        "awslogs-stream-prefix": "dummyapi-staging-service",
+        "awslogs-group": "${prefix}-log-group"
+      }
+    },
+    "portMappings": [
+      {
+        "containerPort": 4000,
+        "hostPort": 4000,
+        "protocol": "tcp"
+      }
+    ],
+    "cpu": 1,
+    "environment": [
+      {
+        "name": "NODE_ENV",
+        "value": "staging"
+      },
+      {
+        "name": "PORT",
+        "value": "4000"
+      }
+    ],
+    "ulimits": [
+      {
+        "name": "nofile",
+        "softLimit": 65536,
+        "hardLimit": 65536
+      }
+    ],
+    "mountPoints": [],
+    "memory": 2048,
+    "volumesFrom": []
+  }
+]
+
+
---
+++ main.tf
@@ -0,0 +1,254 @@
+provider "aws" {
+  region     = "us-east-1"
+#  access_key = "my-access-key"
+#  secret_key = "my-secret-key"
+}
+
+variable "prefix" {
+    description = "prefix prepended to names of all resources created"
+    default = "aws-terraform-test"
+}
+
+variable "region" {
+    description = "selects the aws region to apply these services to"
+    default = "us-east-1"
+}
+
+variable "source_path" {
+  description = "source path for project"
+  default     = "./project"
+}
+
+variable "tag" {
+  description = "tag to use for our new docker image"
+  default     = "latest"
+}
+
+resource "aws_ecs_cluster" "staging" {
+  name = "${var.prefix}-cluster"
+}
+
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+resource "aws_security_group" "lb" {
+  name        = "${var.prefix}-lb-sg"
+  description = "controls access to the Application Load Balancer (ALB)"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ecs_tasks" {
+  name        = "${var.prefix}-tasks-sg"
+  description = "allow inbound access from the ALB only"
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 4000
+    to_port         = 4000
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.lb.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "staging" {
+  name               = "${var.prefix}-alb"
+  subnets            = data.aws_subnet_ids.default.ids
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb.id]
+
+  tags = {
+    Environment = "staging"
+    Application = "${var.prefix}-app"
+  }
+}
+
+resource "aws_lb_listener" "https_forward" {
+  load_balancer_arn = aws_lb.staging.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.staging.arn
+  }
+}
+
+resource "aws_lb_target_group" "staging" {
+  name        = "${var.prefix}-alb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "90"
+    protocol            = "HTTP"
+    matcher             = "200-299"
+    timeout             = "20"
+    path                = "/"
+    unhealthy_threshold = "2"
+  }
+}
+
+resource "aws_ecr_repository" "repo" {
+  name = "${var.prefix}/runner"
+}
+
+resource "aws_ecr_lifecycle_policy" "repo-policy" {
+  repository = aws_ecr_repository.repo.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep image deployed with tag latest",
+      "selection": {
+        "tagStatus": "tagged",
+        "tagPrefixList": ["latest"],
+        "countType": "imageCountMoreThan",
+        "countNumber": 1
+      },
+      "action": {
+        "type": "expire"
+      }
+    },
+    {
+      "rulePriority": 2,
+      "description": "Keep last 2 any images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 2
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
+data "aws_iam_policy_document" "ecs_task_execution_role" {
+  version = "2012-10-17"
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "${var.prefix}-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+data "template_file" "sproutlyapp" {
+  template = file("./app.json.tpl")
+  vars = {
+    aws_ecr_repository = aws_ecr_repository.repo.repository_url
+    tag                = "latest"
+    app_port           = 80
+    region             = "${var.region}"
+    prefix             = "${var.prefix}"
+  }
+}
+
+resource "aws_ecs_task_definition" "service" {
+  family                   = "${var.prefix}-task-family"
+  network_mode             = "awsvpc"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  cpu                      = 256
+  memory                   = 2048
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = data.template_file.sproutlyapp.rendered
+  tags = {
+    Environment = "staging"
+    Application = "${var.prefix}-app"
+  }
+}
+
+resource "aws_ecs_service" "staging" {
+  name            = "${var.prefix}-service"
+  cluster         = aws_ecs_cluster.staging.id
+  task_definition = aws_ecs_task_definition.service.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_tasks.id]
+    subnets          = data.aws_subnet_ids.default.ids
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.staging.arn
+    container_name   = "${var.prefix}-app"
+    container_port   = 4000
+  }
+
+  depends_on = [aws_lb_listener.https_forward, aws_iam_role_policy_attachment.ecs_task_execution_role]
+
+  tags = {
+    Environment = "staging"
+    Application = "${var.prefix}-app"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "dummyapi" {
+  name = "${var.prefix}-log-group"
+
+  tags = {
+    Environment = "staging"
+    Application = "${var.prefix}-app"
+  }
+}
+
+// example -> ./push.sh . 123456789012.dkr.ecr.us-west-1.amazonaws.com/hello-world latest
+
+resource "null_resource" "push" {
+  provisioner "local-exec" {
+     command     = "${coalesce("push.sh", "${path.module}/push.sh")} ${var.source_path} ${aws_ecr_repository.repo.repository_url} ${var.tag}"
+     interpreter = ["bash", "-c"]
+  }
+}
+
